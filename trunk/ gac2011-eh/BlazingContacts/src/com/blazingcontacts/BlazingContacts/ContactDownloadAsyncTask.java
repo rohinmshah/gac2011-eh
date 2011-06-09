3fd920b5fb766e74953959925847e551f733020f
rohinmshah@gmail.com <rohinmshah@gmail.com@13405d40-5787-4b20-b4e5-f3c8254300fc>
2011-06-09 21:25:46 +0000

    Changed the AsyncTask from debug mode to normal mode. Previous commit created the AsyncTask and the WebTestWrapper, used for testing the app without the web service.

--- trunk/ gac2011-eh/BlazingContacts/src/com/blazingcontacts/BlazingContacts/ContactDownloadAsyncTask.java
+++ trunk/ gac2011-eh/BlazingContacts/src/com/blazingcontacts/BlazingContacts/ContactDownloadAsyncTask.java
@@ -13,7 +13,7 @@ public class ContactDownloadAsyncTask extends
 		AsyncTask<Intent, GroupStatus, Void> {
 
 	private ContactDownloadService mService;
-	private WebTestWrapper mWrapper;
+	private WebWrapper mWrapper;
 	private GroupStatus mStatus;
 	private Notification mNotification;
 	private NotificationManager mManager;
@@ -47,7 +47,7 @@ public class ContactDownloadAsyncTask extends
 				Date time = WebWrapper.getDateFromNow(extras
 						.getLong(StartActivity.LIFETIME_MILLIS));
 				int maxPeople = extras.getInt(StartActivity.MAX_PEOPLE);
-				mWrapper = new WebTestWrapper(groupName, password, user, time,
+				mWrapper = new WebWrapper(groupName, password, user, time,
 						maxPeople);
 			} catch (Exception e) {
 				contentTitle = "Connection failed";
@@ -62,7 +62,7 @@ public class ContactDownloadAsyncTask extends
 
 		case GroupActivity.TYPE_JOIN:
 			try {
-				mWrapper = new WebTestWrapper(groupName, password, user);
+				mWrapper = new WebWrapper(groupName, password, user);
 			} catch (Exception e) {
 				contentTitle = "Connection failed";
 				contentText = "Could not connect to the server.  Aborting.";
