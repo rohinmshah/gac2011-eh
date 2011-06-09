61ae0e07698bc987319d105893be9c811ce00f13
Samnsparky <Samnsparky@13405d40-5787-4b20-b4e5-f3c8254300fc>
2011-06-09 06:03:14 +0000

    Added back WebWrapper

---  gac2011-eh/BlazingContacts/src/com/blazingcontacts/BlazingContacts/JoinActivity.java
+++  gac2011-eh/BlazingContacts/src/com/blazingcontacts/BlazingContacts/JoinActivity.java
@@ -1,29 +1,35 @@
 package com.blazingcontacts.BlazingContacts;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.TextView;
 
-public class JoinActivity extends Activity {
-    /** Called when the activity is first created. */
+public class JoinActivity extends GroupActivity {
+	/** Called when the activity is first created. */
 	Button join_begin;
-	
-	   @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.joinview);
-	        
-	        join_begin = (Button) findViewById(R.id.join_begin);
-	        join_begin.setOnClickListener(new OnClickListener() {
-		        public void onClick (View v) {
-		        	Intent i = new Intent (getApplicationContext(), MainActivity.class);
-		        	startActivity(i);
-	        }
-	        });
-	    } 
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.joinview);
+
+		join_begin = (Button) findViewById(R.id.join_begin);
+		join_begin.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(),
+						MainActivity.class);
+				startActivity(i);
+			}
+		});
+	}
+
+	@Override
+	protected void populateContact(String name, String phoneNumber, String email) {
+		((TextView) findViewById(R.id.nameCB)).setText(name);
+		((TextView) findViewById(R.id.phoneNumberCB)).setText(phoneNumber);
+		((TextView) findViewById(R.id.emailCB)).setText(email);
+	}
 }
