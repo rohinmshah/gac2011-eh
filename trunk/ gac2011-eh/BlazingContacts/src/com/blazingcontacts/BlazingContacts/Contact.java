49de65ac85a26debbc22821a420b88d175b1eafe
Samnsparky <Samnsparky@13405d40-5787-4b20-b4e5-f3c8254300fc>
2011-06-09 05:12:57 +0000

    Added updates to WebWrapper, Contact, and GroupStatus before WebWrapper unit testing

--- trunk/ gac2011-eh/BlazingContacts/src/com/blazingcontacts/BlazingContacts/Contact.java
+++ trunk/ gac2011-eh/BlazingContacts/src/com/blazingcontacts/BlazingContacts/Contact.java
@@ -1,5 +1,6 @@
 package com.blazingcontacts.BlazingContacts;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -8,6 +9,9 @@ import org.json.JSONObject;
  */
 public class Contact {
 
+	private static final String JSON_NAME_ATTRIBUTE = "name";
+	private static final String JSON_PHONE_ATTRIBUTE = "phone";
+	private static final String JSON_EMAIL_ATTRIBUTE = "email";
 	private String name, phoneNumber, email;
 
 	/**
@@ -49,11 +53,33 @@ public class Contact {
 
 	/**
 	 * Returns a JSON encoded version of this contact
-	 * @return A JSONObject representation of this object
+	 * @return A JSON encoded string representation of this object
+	 * @throws JSONException 
 	 */
-	public JSONObject toJSON() {
-		// TODO Auto-generated method stub
-		return null;
+	public String toJSON() throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put(JSON_NAME_ATTRIBUTE, name);
+		json.put(JSON_PHONE_ATTRIBUTE, phoneNumber);
+		json.put(JSON_EMAIL_ATTRIBUTE, email);
+		return json.toString();
+	}
+
+	/**
+	 * Creates a new contact by through the provided JSON object
+	 * @param json JSON encoded string
+	 * @return Contact instance from json
+	 * @throws JSONException 
+	 */
+	public static Contact fromJSON(String json) throws JSONException {
+		
+		// Parse JSON
+		JSONObject jsonObject = new JSONObject(json);
+		String name = jsonObject.getString(JSON_NAME_ATTRIBUTE);
+		String phoneNumber = jsonObject.getString(JSON_PHONE_ATTRIBUTE);
+		String email = jsonObject.getString(JSON_EMAIL_ATTRIBUTE);
+		
+		
+		return new Contact(name, phoneNumber, email);
 	}
 
 }
